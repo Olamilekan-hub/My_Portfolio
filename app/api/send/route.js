52b30b0086f49cea470de77e82caf86b770f157c
Olamilekan <adebukolalamilekan123@gmail.com>
2024-01-30 12:36:32 -1100

    update

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,27 +1,27 @@
+//import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
-
 export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
+  // const{body} = await req.json();
+  const{email,subject,message} = await req.json();
   try {
     const data = await resend.emails.send({
       from: fromEmail,
-      to: ["adebukolaolamilekan123@gmail.com", email],
+      to: ["adebukolaolamilekan123@gmail.com",email],
       subject: 'Hello world',
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank You for contacting us !</p>
-          <p>New message submitted from {email}</p>
-          <p>{message}</p>
-        </>
-      ),
+      react: 
+      <>
+      <h1>{subject}</h1>
+      <p>Thank You for contacting us !</p>
+      <p>New message submitted from {email}</p>
+      <p>{message}</p>
+      </>,
     });
 
-    return res.json(data);
+    return Response.json(data);
   } catch (error) {
-    return res.json({ error });
+    return Response.json({ error });
   }
 }
